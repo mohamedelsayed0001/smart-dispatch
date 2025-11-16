b7a4bc5b3b2e2a9a13bf842e405cb76ef272494b
mohamedelsayed0001 <mohamedelsayyed234@gmail.com>
2025-11-16 14:24:13 +0200

    Initial commit: Add frontend and backend for Smart Dispatch System

---
+++ smart-dispatch-system/src/main/java/com/smartdispatch/smart_dispatch_system/SmartDispatchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smartdispatch.smart_dispatch_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDispatchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDispatchSystemApplication.class, args);
+	}
+
+}
---
+++ smart-dispatch-system/src/test/java/com/smartdispatch/smart_dispatch_system/SmartDispatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdispatch.smart_dispatch_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDispatchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
