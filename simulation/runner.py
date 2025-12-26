4753110940043dff9b1b5574abc6ee2518dbda57
Mahmoud Hesham <62242454+mahmoud842@users.noreply.github.com>
2025-12-26 21:42:11 +0200

    feat: Add initial loadUsers and runner scripts with TODOs for vehicle operator management

---
+++ simulation/runner.py
@@ -0,0 +1,5 @@
+# TODO fetch vehicle operators that has name _test_1 -> _test_500
+# TODO get token for the choosen operators
+# TODO run on each seperate thread the vehicle opeartor
+# TODO 5 Users account to make incidents
+# TODO USER thread to make incidents
